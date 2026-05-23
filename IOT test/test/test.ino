a78b22d2aacacaeedecf1183f595b2074eef0a9a
Dulminashehan <dulminashehan596@gmail.com>
2026-05-23 21:11:59 +0530

    Update test.ino

--- IOT test/test/test.ino
+++ IOT test/test/test.ino
@@ -1,22 +1,29 @@
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
 
+// ================= WIFI =================
 #define WIFI_SSID "Dulmina"
 #define WIFI_PASSWORD "dula1790"
 
+// ================= FIREBASE =================
 #define API_KEY "AIzaSyAFrR3ykHZf48g-JrjHfPkzqBzaCDOXdVs"
-#define DATABASE_URL "https://forestmonitoringsystem-44d84-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define DATABASE_URL "https://forestmonitoringsystem-44d84-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
+// ================= OBJECTS =================
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-void setup() {
+Adafruit_AMG88xx amg;
+float pixels[64];
 
+void setup() {
   Serial.begin(115200);
 
+  // ================= WIFI CONNECT =================
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
   Serial.print("Connecting to WiFi");
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -24,27 +31,67 @@ void setup() {
     delay(300);
   }
 
-  Serial.println();
-  Serial.println("Connected!");
+  Serial.println("\nWiFi Connected!");
 
+  // ================= FIREBASE SETUP =================
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  // ================= AMG8833 INIT =================
+  Wire.begin(21, 22);
+
+  if (!amg.begin()) {
+    Serial.println("AMG8833 NOT FOUND!");
+    while (1);
+  }
+
+  Serial.println("AMG8833 Ready!");
 }
 
 void loop() {
 
-  int temp = random(25, 60);
+  // ================= READ SENSOR =================
+  amg.readPixels(pixels);
 
-  if (Firebase.RTDB.setInt(&fbdo, "/forest/temperature", temp)) {
-    Serial.println("Data Sent");
+  float maxTemp = 0;
+
+  // find hottest point
+  for (int i = 0; i < 64; i++) {
+    if (pixels[i] > maxTemp) {
+      maxTemp = pixels[i];
+    }
   }
-  else {
-    Serial.println("Failed");
+
+  Serial.print("Max Temp: ");
+  Serial.println(maxTemp);
+
+  // ================= FIRE DETECTION LOGIC =================
+  String fireStatus;
+
+  if (maxTemp > 60) {
+    fireStatus = "FIRE";
+    Serial.println("🔥 FIRE DETECTED!");
+  } else {
+    fireStatus = "SAFE";
+    Serial.println("✅ SAFE");
+  }
+
+  // ================= SEND TO FIREBASE =================
+
+  if (Firebase.RTDB.setFloat(&fbdo, "/forest/maxTemperature", maxTemp)) {
+    Serial.println("Temperature Sent");
+  } else {
     Serial.println(fbdo.errorReason());
   }
 
-  delay(5000);
+  if (Firebase.RTDB.setString(&fbdo, "/forest/fireStatus", fireStatus)) {
+    Serial.println("Fire Status Sent");
+  } else {
+    Serial.println(fbdo.errorReason());
+  }
+
+  delay(3000);
 }
