f76d440a444803b445414a78bcc0c8d694c22609
Dulminashehan <dulminashehan596@gmail.com>
2026-05-28 12:49:00 +0530

    robo eyes

--- IOT test/test/oled/oled.ino
+++ IOT test/test/oled/oled.ino
@@ -3,18 +3,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-// Display size
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-// SPI Pins
+// OLED SPI Pins
 #define OLED_MOSI 23
 #define OLED_CLK  18
 #define OLED_DC   2
 #define OLED_CS   5
 #define OLED_RESET 4
 
-// SH1106 display
 Adafruit_SH1106G display = Adafruit_SH1106G(
   SCREEN_WIDTH,
   SCREEN_HEIGHT,
@@ -24,33 +22,44 @@ Adafruit_SH1106G display = Adafruit_SH1106G(
   OLED_CS
 );
 
+void drawEyes(int eyeX) {
+
+  display.clearDisplay();
+
+  // Left eye
+  display.fillRoundRect(15, 15, 40, 30, 8, SH110X_WHITE);
+
+  // Right eye
+  display.fillRoundRect(73, 15, 40, 30, 8, SH110X_WHITE);
+
+  // Pupils
+  display.fillCircle(30 + eyeX, 30, 6, SH110X_BLACK);
+  display.fillCircle(88 + eyeX, 30, 6, SH110X_BLACK);
+
+  display.display();
+}
+
 void setup() {
 
   Serial.begin(115200);
 
-  // Start display
   if(!display.begin(0, true)) {
-    Serial.println("Display not found");
+    Serial.println("OLED failed");
     while(1);
   }
-
-  // Clear screen
-  display.clearDisplay();
-
-  // Text settings
-  display.setTextSize(3);
-  display.setTextColor(SH110X_WHITE);
-
-  // Position
-  display.setCursor(10, 20);
-
-  // Print text
-  display.println("HELLO");
-
-  // Show on OLED
-  display.display();
 }
 
 void loop() {
 
+  // Look left
+  drawEyes(-5);
+  delay(500);
+
+  // Look center
+  drawEyes(0);
+  delay(500);
+
+  // Look right
+  drawEyes(5);
+  delay(500);
 }
