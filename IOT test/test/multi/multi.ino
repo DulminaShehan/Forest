961fdd1ccf8a2400915c4a7f4c4c4de30d5d9af8
Dulminashehan <dulminashehan596@gmail.com>
2026-06-03 20:12:21 +0530

    update

--- IOT test/test/multi/multi.ino
+++ IOT test/test/multi/multi.ino
@@ -1,75 +1,40 @@
-#include <DHT.h>
-
-#define DHTPIN           4
-#define DHTTYPE          DHT22
-
-#define RAIN_DIGITAL_PIN 27
-#define RAIN_ANALOG_PIN  33
-
-#define MQ2_ANALOG_PIN   34
-#define MQ9_ANALOG_PIN   35
-
-DHT dht(DHTPIN, DHTTYPE);
-
-int averageRead(int pin) {
-  long total = 0;
-
-  for (int i = 0; i < 20; i++) {
-    total += analogRead(pin);
-    delay(5);
-  }
-
-  return total / 20;
-}
+#define POWER_PIN 19
+#define AO_PIN 36
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(RAIN_DIGITAL_PIN, INPUT);
-
-  analogReadResolution(12);
-
-  dht.begin();
-
-  Serial.println("Sensor Test Started");
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW); // Sensor OFF initially
 }
 
 void loop() {
+  digitalWrite(POWER_PIN, HIGH); // Turn sensor ON
+  delay(10);
 
-  float temp = dht.readTemperature();
-  float hum  = dht.readHumidity();
+  int rainValue = analogRead(AO_PIN);
 
-  int mq2  = averageRead(MQ2_ANALOG_PIN);
-  int mq9  = averageRead(MQ9_ANALOG_PIN);
-  int rain = averageRead(RAIN_ANALOG_PIN);
+  digitalWrite(POWER_PIN, LOW); // Turn sensor OFF
 
-  bool rainDetected = digitalRead(RAIN_DIGITAL_PIN) == LOW;
+  int rainPercent = map(rainValue, 4095, 0, 0, 100);
 
-  Serial.println("--------------------------------");
+  Serial.print("Rain Value: ");
+  Serial.print(rainValue);
 
-  Serial.print("Temperature: ");
-  Serial.print(temp);
-  Serial.println(" C");
+  Serial.print(" | Rain Percentage: ");
+  Serial.print(rainPercent);
+  Serial.println("%");
 
-  Serial.print("Humidity: ");
-  Serial.print(hum);
-  Serial.println(" %");
+  if (rainPercent < 20) {
+    Serial.println("Status: No Rain");
+  }
+  else if (rainPercent < 60) {
+    Serial.println("Status: Light Rain");
+  }
+  else {
+    Serial.println("Status: Heavy Rain");
+  }
 
-  Serial.print("MQ2 Value: ");
-  Serial.println(mq2);
-
-  Serial.print("MQ9 Value: ");
-  Serial.println(mq9);
-
-  Serial.print("Rain Analog: ");
-  Serial.println(rain);
-
-  Serial.print("Rain Status: ");
-
-  if (rainDetected)
-    Serial.println("RAIN DETECTED");
-  else
-    Serial.println("NO RAIN");
-
-  delay(2000);
+  Serial.println("--------------------");
+  delay(1000);
 }
